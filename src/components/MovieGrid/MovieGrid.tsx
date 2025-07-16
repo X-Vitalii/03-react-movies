ff54de88d4283bc48c46d824f6fe963e60477e8f
Vitaliy Khyzhnyak <vit.khyzhn@gmail.com>
2025-07-17 01:35:41 +0300

    complete project - minor correct

--- src/components/MovieGrid/MovieGrid.tsx
+++ src/components/MovieGrid/MovieGrid.tsx
@@ -1,4 +1,4 @@
-import { Movie } from "../../types/movie";
+import { type Movie } from "../../types/movie";
 import styles from "./MovieGrid.module.css";
 import { getPosterUrl } from "../../utils/getPosterUrl";
 
